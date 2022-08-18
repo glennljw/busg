23292c060278e6d0828428e315bafaa6e8472edb
glennljw <glennljw7@gmail.com>
2022-08-18 20:01:24 +0800

    Update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Head>
-        <title>SG Bus</title>
+        <title>Busg</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <Component {...pageProps} />
